414d588952ca0054179cc2080b1a29d885a435b9
bpohoriletz <bod-lv@bigmir.net>
2010-03-08 18:30:17 +0200

    fixed bugs in blog

--- photo-portfolio/app/controllers/application_controller.rb
+++ photo-portfolio/app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 
 class ApplicationController < ActionController::Base
   include SimpleCaptcha::ControllerHelpers
+  include AuthenticatedSystem
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
