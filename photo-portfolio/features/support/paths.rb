d8aaae19b5acc1d2e9b8df2766ae5ac26fc296c8
bpohoriletz <bod-lv@bigmir.net>
2010-03-12 13:44:58 +0200

    Signed-off-by: bpohoriletz <bod-lv@bigmir.net>

--- photo-portfolio/features/support/paths.rb
+++ photo-portfolio/features/support/paths.rb
@@ -10,8 +10,6 @@ module NavigationHelpers
     
     when /the home\s?page/
       '/'
-    when /edit information page/
-     '/contacts/new'
     
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
