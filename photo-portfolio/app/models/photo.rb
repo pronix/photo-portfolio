f45179ac341fce72e7c10fe638869d4b7946455a
bpohoriletz <bod-lv@bigmir.net>
2010-03-17 16:30:11 +0200

    removed some old files

--- photo-portfolio/app/models/photo.rb
+++
@@ -1,7 +0,0 @@
-class Photo < ActiveRecord::Base
-  belongs_to :album
-  validates_presence_of :name
-  has_attached_file :image,:styles => { :medium =>"800x800",:thumb => "140x140" }
-  validates_attachment_size :image, :less_than => 5.megabytes
-  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
-end
--- photo-portfolio/db/migrate/20100317081129_add_attachments__to_pictures.rb
+++
@@ -1,9 +0,0 @@
-class AddAttachments_toPictures < ActiveRecord::Migration
-  def self.up
-
-  end
-
-  def self.down
-
-  end
-end
